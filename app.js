2474b41fb6f7a628a6380a74976f0e20bc7b204e
Lewis Youl <lewisyoul@gmail.com>
2019-07-21 10:26:32 +0100

    [feature] Add timeout to poke app!

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
+const https = require('https')
 const express = require('express');
 const port = process.env.PORT || 3000;
 
 const app = express();
 
 app.get('/', (req, res) => {
+  console.log('you got me')
   res.send(JSON.stringify({ Hello: 'World' }));
 });
 
 app.listen(port, () => {
   console.log(`keepalive is listening on port ${port}!`);
 });
+
+setInterval(() => {
+  https.get("https://glacial-gorge-38769.herokuapp.com/");
+}, 300000);
